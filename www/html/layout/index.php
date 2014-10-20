19e9a5f635e536ccd0350b8d2bd9557cadf3088b
sleeping-lion <toughjjh@gmail.com>
2014-10-21 03:53:01 +0900

    update

--- www/html/layout/index.php
+++ www/html/layout/index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>SL BOARD</title>
-	<meta charset="utf-8">
-	<link type="text/css" rel="stylesheet" href="/stylesheets/index.css" />
-	<link type="text/css" rel="stylesheet" href="/stylesheets/bootstrap.min.css" />	
-	<?php sl_style($sl_style); ?>
-	<meta content="width=device-width, initial-scale=1.0" name="viewport" />
-	<meta name="author" content="Sleeping-Lion" />
-	<!--[if IE]>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <![endif]-->	
+<title>SL BOARD</title>
+<meta charset="utf-8">
+<link type="text/css" rel="stylesheet" href="/stylesheets/index.css" />
+<link type="text/css" rel="stylesheet" href="/stylesheets/bootstrap.min.css" />
+<?php sl_style($config['template']['theme_style']); ?>	
+<meta content="width=device-width, initial-scale=1.0" name="viewport" />
+<meta name="author" content="Sleeping-Lion" />
+<!--[if IE]>
+ <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<![endif]-->
 </head>
 <body id="sl_main_page">
 	<?php require_once $config['template']['header'] ?>
@@ -27,8 +27,8 @@
 		</div>		
 	</div>
 	<?php require_once $config['template']['footer'] ?>
-	<script type="text/javascript" src="/javascripts/jquery-2.1.1.min.js"></script>
-	<script type="text/javascript" src="/javascripts/bootstrap.min.js" ></script>
-	<?php sl_js($sl_js); ?>		
+<script type="text/javascript" src="/javascripts/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="/javascripts/bootstrap.min.js" ></script>
+<?php sl_js($config['template']['theme_script']); ?>	
 </body>
 </html>
