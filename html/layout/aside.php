d6cfa13b09a661d4115a67ddd125503b89f28245
sleeping-lion <toughjjh@gmail.com>
2014-11-17 03:37:24 +0900

    update

--- html/layout/aside.php
+++ html/layout/aside.php
@@ -1,5 +1,5 @@
 <aside class="visible-lg">
-  <?php require_once BOARD_HTML_DIRECTORY.DIRECTORY_SEPARATOR.'blog_categories'.DIRECTORY_SEPARATOR.'_aside.php' ?>
-  <?php require_once BOARD_HTML_DIRECTORY.DIRECTORY_SEPARATOR.'blogs'.DIRECTORY_SEPARATOR.'_tag_cloud.php' ?>
+  <?php require_once find_html($sl_theme,'_aside.php','blog_categories') ?>
+  <?php require_once find_html($sl_theme,'_tag_cloud.php','blogs') ?>
 	<div class="clearfix"></div>
 </aside>
